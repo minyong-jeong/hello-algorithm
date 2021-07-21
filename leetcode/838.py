820124d5f3ab19210c8b9447df2e747920aaab2c
Minyong Jeong <jmy3155@gmail.com>
2021-07-22 00:15:58 +0900

    Add 838. Push Dominoes (Medium)

---
+++ leetcode/838.py
@@ -0,0 +1,32 @@
+"""
+    https://leetcode.com/problems/push-dominoes/
+    838. Push Dominoes (Medium)
+"""
+
+class Solution(object):
+    def pushDominoes(self, dominoes):
+        """
+        :type dominoes: str
+        :rtype: str
+        """
+        d = ['L'] + list(dominoes) + ['R']
+
+        pre_char, pre_loc = 'L', -1
+        for i in range(len(d)):
+            if d[i] != '.':
+                if pre_char == d[i]:
+                    self.update(d, pre_loc+1, i, d[i])
+                elif pre_char > d[i]:
+                    mid = int((pre_loc+i)/2)
+                    remain = (pre_loc+i)%2
+                    self.update(d, pre_loc+1, mid+remain, 'R')
+                    self.update(d, mid+1, i, 'L')
+                pre_char = d[i]
+                pre_loc = i
+        
+        d = d[1:-1]
+        return ''.join(d)
+                    
+    def update(self, d, left, right, c):
+        for i in range(left, right):
+            d[i] = c
